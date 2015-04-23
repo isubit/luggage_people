e6e572c6221a676c19bcd18fa19d02de2ccda46e
rlfrahm <frahmryan@gmail.com>
2015-04-23 16:54:36 -0500

    Adding url from author to profile

--- luggage_people_authors/luggage_people_authors_default.tpl.php
+++ luggage_people_authors/luggage_people_authors_default.tpl.php
@@ -1,10 +1,10 @@
 <div class="media">
     <div class="media-left">
-        <a href>
+        <a href="<?php print $GLOBALS['base_url']; ?>/node/<?php print $url; ?>">
             <img src="<?php print $image_src; ?>">
         </a>
     </div>
     <div class="media-body">
-        <h4 class="media-heading"><?php print $firstname; ?> <?php print $lastname; ?> <small><em><?php print $position; ?></em></small></h4>
+        <h4 class="media-heading"><a href="<?php print $GLOBALS['base_url']; ?>/node/<?php print $url; ?>"><?php print $firstname; ?> <?php print $lastname; ?></a> <small><em><?php print $position; ?></em></small></h4>
     </div>
 </div>
