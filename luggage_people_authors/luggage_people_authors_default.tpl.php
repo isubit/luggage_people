bac9f6a44928646bea3cadf0295f9a05def360f0
rlfrahm <frahmryan@gmail.com>
2015-04-23 16:44:42 -0500

    Adding field format for authors taxonomy term reference field

---
+++ luggage_people_authors/luggage_people_authors_default.tpl.php
@@ -0,0 +1,10 @@
+<div class="media">
+    <div class="media-left">
+        <a href>
+            <img src="<?php print $image_src; ?>">
+        </a>
+    </div>
+    <div class="media-body">
+        <h4 class="media-heading"><?php print $firstname; ?> <?php print $lastname; ?> <small><em><?php print $position; ?></em></small></h4>
+    </div>
+</div>
