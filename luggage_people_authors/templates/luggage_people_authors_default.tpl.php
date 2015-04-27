c67ec6c0613949158c29fd35883831c53db50a3a
rlfrahm <frahmryan@gmail.com>
2015-04-27 10:39:13 -0500

    Rerolling feature

--- luggage_people_authors/templates/luggage_people_authors_default.tpl.php
+++ luggage_people_authors/templates/luggage_people_authors_default.tpl.php
@@ -1,10 +1,9 @@
-<div class="media">
+<a href="<?php print $GLOBALS['base_url']; ?>/node/<?php print $url; ?>" class="media">
     <div class="media-left">
-        <a href="<?php print $GLOBALS['base_url']; ?>/node/<?php print $url; ?>">
-            <img src="<?php print $image_src; ?>">
-        </a>
+        <img src="<?php print $image_src; ?>">
     </div>
     <div class="media-body">
-        <h4 class="media-heading"><a href="<?php print $GLOBALS['base_url']; ?>/node/<?php print $url; ?>"><?php print $firstname; ?> <?php print $lastname; ?></a> <small><em><?php print $position; ?></em></small></h4>
+        <h4 class="media-heading"><?php print $firstname; ?> <?php print $lastname; ?> <small><em><?php print $position; ?></em></small></h4>
+        <small><?php print $bio; ?></small>
     </div>
-</div>
+</a>
